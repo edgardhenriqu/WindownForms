6f417bd7f7a14551bbed8d74ac2fe68f4a20bc47
Edgard Henrique <edigarhenriqu@gmail.com>
2023-07-28 11:44:08 -0300

    Capturando o mouse no formulário

---
+++ WindowsForms/CursoWindowsForms/Frm_Menu_Flutuante.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_Menu_Flutuante : Form
+    {
+        public Frm_Menu_Flutuante()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_Menu_Flutuante_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                MessageBox.Show("Cliquei com o botão da direta do mouse");
+            }
+        }
+    }
+}
